7cfbab5afc192fa6334897b676c6fa3ae09bb397
Towaris4 <ivan.tarasov2488@gmail.com>
2026-05-17 21:41:36 +0300

    3. Доменная модель проекта "АвтоМаг" [#504897]

--- src/main/resources/db/scripts/001_ddl_create_initial_schema.sql
+++ src/main/resources/db/scripts/001_ddl_create_initial_schema.sql
@@ -1,8 +1,21 @@
 CREATE TABLE ads (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     car_brand VARCHAR(100) NOT NULL,
     body_type VARCHAR(50) NOT NULL,
     photo_url VARCHAR(500),
-    is_sold BOOLEAN DEFAULT FALSE,
+    is_sold BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE auto_user (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE auto_post (
+    id SERIAL PRIMARY KEY,
+    description TEXT,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    auto_user_id INT NOT NULL REFERENCES auto_user(id)
 );
